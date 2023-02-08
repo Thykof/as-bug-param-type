fd3419b886a873382bb8fc34ccae7ba6ccabd553
Nathan Seva <thykof@protonmail.ch>
2023-02-08 17:14:25 +0100

    remove noise

--- assembly/__tests__/index.spec.ts
+++ assembly/__tests__/index.spec.ts
@@ -2,7 +2,6 @@ import { addAnything } from "..";
 
 describe("a test group", () => {
   test("the meaning of life", () => {
-    const arrayU8 = [<u8>54];
-    addAnything(arrayU8);
+    addAnything([<u8>54]);
   });
 });
